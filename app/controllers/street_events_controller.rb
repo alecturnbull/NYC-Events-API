f64f6d29c484613e027db86f81a586885ce8ee26
Alec Turnbull <alec@threering.com>
2012-06-16 14:32:16 -0400

    borough search added

--- app/controllers/street_events_controller.rb
+++ app/controllers/street_events_controller.rb
@@ -3,10 +3,24 @@ class StreetEventsController < ApplicationController
 def search
   start_date = Chronic.parse(params[:start_date])
   end_date = Chronic.parse(params[:end_date])
-  if start_date.nil? && end_date.nil?
-    @events = StreetEvent.all
-  else
+  lat = params[:lat]
+  lng = params[:lng]
+
+
+  if params[:start_date] and params[:end_date] and params[:borough]
+    borough = params[:borough].capitalize
+    @events = StreetEvent.where("start_date <= ? AND end_date >= ? AND borough = ?", start_date, end_date, borough)
+  elsif params[:start_date] and params[:end_date] and not params[:borough]
     @events = StreetEvent.where("start_date <= ? AND end_date >= ?", start_date, end_date)
+  elsif params[:start_date] and not params[:end_date] and not params[:borough]
+    @events = StreetEvent.where("start_date <= ?", start_date)
+  elsif not params[:start_date] and params[:end_date] and not params[:borough]
+    @events = StreetEvent.where("end_date >= ?", end_date)    
+  elsif not params[:start_date] and not params[:end_date] and params[:borough]
+    borough = params[:borough].capitalize
+    @events = StreetEvent.where("borough = ?", borough)
+  else
+    @events = StreetEvent.all
   end
   respond_to do |format|
     format.json { render :json => @events }
--- app/models/street_event.rb
+++ app/models/street_event.rb
@@ -1,3 +1,5 @@
 class StreetEvent < ActiveRecord::Base
   attr_accessible :address, :borough, :end_date, :event_type, :name, :start_date
+
+
 end
