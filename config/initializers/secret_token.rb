523904eb1a60fa86b59cb6cede502f9c5d1d4bf9
Alec Turnbull <alec@threering.com>
2012-06-16 10:47:10 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HacknJill::Application.config.secret_token = 'baefb4435ac147332044ea789d9d5098104e1b8699ff9aa652076b4bd01597d6895835a59f18a451bd8ae42ac10fd13859e016e33c5e67ec0e2377b94aac5e96'
